8d480ed4842c90f706bc0cdeb47937b27614759b
arafathossain05 <arafat240605@gmail.com>
2026-03-29 22:16:55 +0600

    update code

--- lab_01.cpp
+++ lab_01.cpp
@@ -1,37 +1,61 @@
 #include <iostream>
-#include <iomanip>  // For setprecision
 using namespace std;
 
-class Student {
+class StudentData {
 private:
-    int marks[5];
-    
+    int totalStudents[5];
+    int passedStudents[5];
+    float percentage[5];
+
 public:
+    // Input data
     void input() {
-        cout << "Enter marks for 5 subjects (0-100): ";
-        for(int i = 0; i < 5; i++) {
-            cin >> marks[i];
-            while(marks[i] < 0 || marks[i] > 100) {
-                cout << "Invalid marks! Enter value between 0-100: ";
-                cin >> marks[i];
-            }
+        cout << "Enter total and passed students for 5 years:\n";
+        for (int i = 0; i < 5; i++) {
+            cout << "Year " << i + 1 << ":\n";
+            cout << "Total Students: ";
+            cin >> totalStudents[i];
+            cout << "Passed Students: ";
+            cin >> passedStudents[i];
         }
     }
-    
-    void percentage() {
-        int sum = 0;
-        for(int i = 0; i < 5; i++) {
-            sum += marks[i];
+
+    // Calculate percentage
+    void calculate() {
+        for (int i = 0; i < 5; i++) {
+            percentage[i] = (float)passedStudents[i] / totalStudents[i] * 100;
         }
-        double percentage = sum / 5.0;  // Use floating point division
-        cout << fixed << setprecision(2);  // Show 2 decimal places
-        cout << "Percentage: " << percentage << "%" << endl;
+    }
+
+    // Display data
+    void display() {
+        cout << "\nYear\tPass %\n";
+        for (int i = 0; i < 5; i++) {
+            cout << i + 1 << "\t" << percentage[i] << "%\n";
+        }
+    }
+
+    // Plot graph (simple text bar chart)
+    void plotGraph() {
+        cout << "\n--- Passing Percentage Graph ---\n";
+        for (int i = 0; i < 5; i++) {
+            cout << "Year " << i + 1 << " : ";
+            int stars = percentage[i] / 2; // scale down
+            for (int j = 0; j < stars; j++) {
+                cout << "*";
+            }
+            cout << " (" << percentage[i] << "%)\n";
+        }
     }
 };
 
 int main() {
-    Student s;
-    s.input();
-    s.percentage();
+    StudentData obj;
+
+    obj.input();
+    obj.calculate();
+    obj.display();
+    obj.plotGraph();
+
     return 0;
 }
